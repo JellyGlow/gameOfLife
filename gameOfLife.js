b0d700e225cf7aa12137eccd257d74d5972f3b56
Claire Gallant <claire.luna.gallant@gmail.com>
2016-11-30 00:10:14 +0000

    added button click handlers

--- gameOfLife.js
+++ gameOfLife.js
@@ -1,43 +1,89 @@
-const rows = 24;
-const cols = 24;
+var rows = 24;
+var cols = 24;
 
-//initialize
+var playing = false;
+
+// initialize
 function initialize() {
-  createTable();
+    createTable();
+    setupControlButtons();
 }
 
-//lay out the board
 function createTable() {
-  var gridContainer = document.getElementById("gridContainer");
-  if (!gridContainer) {
-  //throw error
-  console.error("Problem: no div for the grid table!");
-}
-
-var table = document.createElement("table");
-
-for (var i = 0; i < rows; i++) {
-    var tr = document.createElement("tr");
-    for (var j = 0; j < cols; j++) {
-      var cell = document.createElement("td");
-      cell.setAttribute("id", i + "_" + j);
-      cell.setAttribute("class", "dead");
-      cell.onclick = cellClickHandler;
-      tr.appendChild(cell);
+    var gridContainer = document.getElementById("gridContainer");
+    if (!gridContainer) {
+        // throw error
+        console.error("Problem: no div for the grid table!");
     }
-    table.appendChild(tr);
-  }
-  gridContainer.appendChild(table);
+
+    //this is the grid! it has two arrays. One for the rows (i) and one for the columns (j).
+    var table = document.createElement("table");
+
+    for (var i = 0; i < rows; i++) {
+        var tr = document.createElement("tr");
+        for (var j = 0; j < cols; j++) {
+            var cell = document.createElement("td");
+            cell.setAttribute("id", i + "_" + j);
+            cell.setAttribute("class", "dead");
+            cell.onclick = cellClickHandler;
+            tr.appendChild(cell);
+        }
+        table.appendChild(tr);
+    }
+    gridContainer.appendChild(table);
 }
+
+//handling the tapdancings
+// ...(clicks)
 
 function cellClickHandler() {
-  var classes = this.getAttribute("class");
-  if (classes.indexOf("live") > -1) {
-    this.setAttribute("class", "dead");
-  } else {
-    this.setAttribute("class", "live");
-  }
+    var classes = this.getAttribute("class");
+    if (classes.indexOf("live") > -1) {
+        this.setAttribute("class", "dead");
+    } else {
+        this.setAttribute("class", "live");
+    }
 }
 
-//start everything
+
+function setupControlButtons() {
+    // button to start
+    var startButton = document.getElementById("start");
+    startButton.onclick = startButtonHandler;
+
+    // button to clear
+    var clearButton = document.getElementById("clear");
+    clearButton.onclick = clearButtonHandler;
+}
+
+// clear the grid
+function clearButtonHandler() {
+    console.log("Clear the game: stop playing, clear the grid");
+    playing = false;
+    var startButton = document.getElementById("start");
+    startButton.innerHTML = "start";
+}
+
+// start/pause/continue the game
+function startButtonHandler() {
+    if (playing) {
+        console.log("Pause the game");
+        playing = false;
+        this.innerHTML = "continue";
+    } else {
+        console.log("Continue the game");
+        playing = true;
+        this.innerHTML = "pause";
+        play();
+    }
+}
+
+// run the life game
+function play() {
+    console.log("Play the game");
+}
+
+
+// start everything
+
 window.onload = initialize;
